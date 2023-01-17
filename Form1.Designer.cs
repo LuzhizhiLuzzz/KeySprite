8414c39891b0ad995a590084d8e00ad013bf5c93
Xushihao <1376094109@qq.com>
2023-01-17 14:12:34 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace KeySprite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "按键设置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "按键间隔";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "触发按键";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "声音提示";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(156, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 25);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(156, 165);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(156, 207);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "开启声音";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 364);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Configuration;
+
+namespace KeySprite
+{
+    public partial class Form1 : Form
+    {
+        private CDD dd;
+        string DLLPath;
+        string selectStr1;
+        string selectStr2;
+        string KeyInterval;
+        string sound;
+        Dictionary<string, int> KeyToCode;
+        Dictionary<int, string> CodeToKey;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            selectStr1 = ConfigurationManager.AppSettings["presskey"].ToString();
+            selectStr2 = ConfigurationManager.AppSettings["start"].ToString();
+            KeyInterval = ConfigurationManager.AppSettings["Interval"].ToString();
+            DLLPath = ConfigurationManager.AppSettings["DLLPath"].ToString();
+            sound = ConfigurationManager.AppSettings["sound"].ToString();
+            //reg_hotkey();
+            //LoadDllFile(DLLPath);
+            //comboBox_Init();
+            //Map_Init();
+            //dd = new CDD();
+
+        }
+
+
+        private void comboBox_Init()
+        {
+            comboBox1.Items.Add("1");
+            comboBox1.Items.Add("Q");
+            comboBox1.Items.Add("F10");
+            comboBox1.Items.Add("F11");
+            comboBox1.Items.Add("F12");
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf(selectStr1);
+            comboBox2.Items.Add("1");
+            comboBox2.Items.Add("Q");
+            comboBox2.Items.Add("F10");
+            comboBox2.Items.Add("F11");
+            comboBox2.Items.Add("F12");
+            comboBox2.SelectedIndex = comboBox2.Items.IndexOf(selectStr2);
+        }
+        private void Map_Init()
+        {
+            KeyToCode = new Dictionary<string, int>();
+            CodeToKey = new Dictionary<int, string>();
+            KeyToCode.Add("F12", 112);
+            KeyToCode.Add("F11", 111);
+            KeyToCode.Add("Q", 301);
+            KeyToCode.Add("F10", 110);
+            KeyToCode.Add("1", 201);
+            CodeToKey.Add(112,"F12");
+            CodeToKey.Add(111, "F11");
+            CodeToKey.Add(110, "F10");
+            CodeToKey.Add(201, "1");
+            CodeToKey.Add(301, "Q");
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectStr1 = comboBox1.SelectedItem.ToString();
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectStr2 = comboBox2.SelectedItem.ToString();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            unreg_hotkey();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            KeyInterval = textBox1.Text;
+        }
+
+        private void LoadDllFile(string dllfile)
+        {
+
+            int ret = dd.Load(dllfile);
+            if (ret != 1) { MessageBox.Show("DD64.DLL Load Error"); return; }
+
+
+            ret = dd.btn(0); //DD Initialize
+            if (ret != 1) { MessageBox.Show("DD64.DLL Initialize Error"); return; }
+
+            return;
+        }
+
+        #region "HotKey"
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(
+         IntPtr hWnd,
+         int id,
+         KeyModifiers modkey,
+         Keys vk
+        );
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(
+         IntPtr hWnd,
+         int id
+        );
+
+        void reg_hotkey()
+        {
+            RegisterHotKey(this.Handle, 80, 0, Keys.F12);
+        }
+
+        void unreg_hotkey()
+        {
+            UnregisterHotKey(this.Handle, 80);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    ProcessHotkey(m);
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void ProcessHotkey(Message msg)
+        {
+            switch (msg.WParam.ToInt32())
+            {
+                case 80:
+                    Fun80();
+                    break;
+                case 90:
+                    Fun90();
+                    break;
+            }
+        }
+
+        private void Fun80()
+        {
+            dd.str("Keyboard char [A-Za_z] {@$} ");
+        }
+
+        private void Fun90()
+        {
+            if (dd.key != null)
+            {
+                //CTRL+ALT+DEL
+                dd.key(600, 1);                                      //600 == L.CTRL down
+                dd.key(602, 1);                                      // ALT   down
+                dd.key(706, 1);                                      // DEL   down
+                System.Threading.Thread.Sleep(5);
+                dd.key(706, 2);                                       //up
+                dd.key(602, 2);
+                dd.key(600, 2);
+            }
+        }
+
+        #endregion
+
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            textBox2.Text = e.KeyData.ToString();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
